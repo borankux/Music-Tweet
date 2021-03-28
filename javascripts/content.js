00ce95706ea8266bf173c9be45953f182b1aa342
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-28 17:20:50 +0900

    refactor: Change indent 4 to 2

--- javascripts/content.js
+++ javascripts/content.js
@@ -12,52 +12,52 @@ document.body.appendChild(canvas);
 X('oscillator').setup([true, true, true, true]);
 
 X('mml').setup({
-    'ended': () => {
-        canvas.setAttribute('hidden', '');
-    },
-    'error': () => {
-        canvas.setAttribute('hidden', '');
-    }
+  'ended': () => {
+    canvas.setAttribute('hidden', '');
+  },
+  'error': () => {
+    canvas.setAttribute('hidden', '');
+  }
 });
 
 X('oscillator').module('analyser').domain('time').setup(canvas).state(true).param({
-    'interval' : 'auto',
-    'wave'     : '#7ac4f7',
-    'grid'     : 'none',
-    'text'     : 'none',
-    'top'      : 0,
-    'left'     : 0,
-    'bottom'   : 0,
-    'right'    : 0
+  'interval': 'auto',
+  'wave'    : '#7ac4f7',
+  'grid'    : 'none',
+  'text'    : 'none',
+  'top'     : 0,
+  'left'    : 0,
+  'bottom'  : 0,
+  'right'   : 0
 });
 
 setTimeout(() => {
-    const tweetboxtimeline = document.querySelector('[contenteditable]');
-    const tweetbuttons     = document.querySelectorAll('[data-testid="tweetButtonInline"]');
+  const tweetboxtimeline = document.querySelector('[contenteditable]');
+  const tweetbuttons     = document.querySelectorAll('[data-testid="tweetButtonInline"]');
 
-    if (tweetbuttons.length > 0) {
-        tweetbuttons[0].addEventListener('click', () => {
-            X('mml').ready(X('oscillator'), [tweetboxtimeline.textContent]).start(0);
-        }, false);
-    }
+  if (tweetbuttons.length > 0) {
+    tweetbuttons[0].addEventListener('click', () => {
+      X('mml').ready(X('oscillator'), [tweetboxtimeline.textContent]).start(0);
+    }, false);
+  }
 }, 5000);
 
 document.addEventListener('mouseup', () => {
-    const selection = window.getSelection();
+  const selection = window.getSelection();
 
-    if (selection.rangeCount < 1) {
-        return;
-    }
+  if (selection.rangeCount < 1) {
+    return;
+  }
 
-    const range = selection.getRangeAt(0);
+  const range = selection.getRangeAt(0);
 
-    X('mml').ready(X('oscillator'), [range.toString()]);
+  X('mml').ready(X('oscillator'), [range.toString()]);
 
-    if (X('mml').get().length < 1) {
-        return;
-    }
+  if (X('mml').get().length < 1) {
+    return;
+  }
 
-    X('mml').start(0);
+  X('mml').start(0);
 
-    X('oscillator').module('session').start();
+  X('oscillator').module('session').start();
 }, false);
--- javascripts/popup.js
+++ javascripts/popup.js
@@ -1,34 +1,34 @@
 'use strict';
 
-document.getElementById('form-wave-type').addEventListener('change', event => {
-    const form = event.currentTarget;
+document.getElementById('form-wave-type').addEventListener('change', (event) => {
+  const form = event.currentTarget;
 
-    let type = '';
+  let type = '';
 
-    for (let i = 0, len = form.elements.length; i < len; i++) {
-        if (form.elements[i].checked) {
-            type = form.elements[i].value;
-            break;
-        }
+  for (let i = 0, len = form.elements.length; i < len; i++) {
+    if (form.elements[i].checked) {
+      type = form.elements[i].value;
+      break;
     }
+  }
 
-    chrome.tabs.executeScript(null, {
-        'code': `
-            for (let i = 0, len = X('oscillator').length(); i < len; i++) {
-                X('oscillator').get(i).param('type', '${type}');
-            }
-        `
-    });
+  chrome.tabs.executeScript(null, {
+    'code': `
+      for (let i = 0, len = X('oscillator').length(); i < len; i++) {
+        X('oscillator').get(i).param('type', '${type}');
+      }
+    `
+  });
 }, false);
 
-document.getElementById('toggle-visualization').addEventListener('click', event => {
-    chrome.tabs.executeScript(null, {
-        'code': `
-            if (${event.currentTarget.checked}) {
-                canvas.removeAttribute('hidden');
-            } else {
-                canvas.setAttribute('hidden', '');
-            }
-        `
-    });
+document.getElementById('toggle-visualization').addEventListener('click', (event) => {
+  chrome.tabs.executeScript(null, {
+    'code': `
+      if (${event.currentTarget.checked}) {
+        canvas.removeAttribute('hidden');
+      } else {
+        canvas.setAttribute('hidden', '');
+      }
+    `
+  });
 }, false);
